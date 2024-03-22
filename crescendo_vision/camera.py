eb92326d3d39c2d186e1419af139c74096d1e0a5
drakeerv <drakeerv@outlook.com>
2024-03-22 13:03:09 -0400

    Revert "Make detection faster"
    This reverts commit e1ca1643744cc1cbc82e535db52dca2c9bb06be2.

--- crescendo_vision/camera.py
+++ crescendo_vision/camera.py
@@ -1,6 +1,6 @@
-import threading
+import io
 import numpy as np
-from picamera2 import Picamera2, Preview
+from picamera2 import Picamera2
 from libcamera import controls
 
 class Camera:
@@ -10,53 +10,33 @@ class Camera:
 
     cam: Picamera2
 
-    def __init__(self, normal_resolution=(640, 480), downscaled_resolution=(320, 240)):
-        self.normal_resolution = normal_resolution
-        self.downscaled_resolution = downscaled_resolution
-
+    def __init__(self):
         self.cam = Picamera2()
-
-        self.frame = None
-        self.thread = None
-        self.running = False
-
-    def capture_thread(self):
-        while self.running:
-            self.frame = self.cam.capture_buffer("lowres")
+        self.cam.resolution = (720, 480)
 
     def start_cap(self):
         '''
         Start capturing video
         '''
-        self.cam.start_preview(Preview.QTGL)
         cfg = self.cam.create_video_configuration(main={
-            'size': self.normal_resolution,
+            'size': (720, 480),
             'format': 'RGB888'
-        }, lowres={
-            'size': self.downscaled_resolution,
-            'format': 'RGB888'
-        })
+            })
         self.cam.configure(cfg)
         self.cam.start()
         self.cam.set_controls({'AfMode': controls.AfModeEnum.Continuous})
-
-        self.running = True
-        self.thread = threading.Thread(target=self.capture_thread)
-        self.thread.start()
     
     def stop_cap(self):
         '''
         Stop capturing video
         '''
 
-        self.running = False
-        self.thread.join()
         self.cam.stop()
-        self.thread = None
     
     def get_frame(self) -> np.ndarray:
         '''
         Grab a numpy array
         '''
 
-        return self.frame
+        arr = self.cam.capture_array()
+        return arr
