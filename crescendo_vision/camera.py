678a86e0247f84679380a416365d1780945c55ff
drakeerv <drakeerv@outlook.com>
2024-03-22 16:34:50 -0400

    Make detection faster V2

--- crescendo_vision/camera.py
+++ crescendo_vision/camera.py
@@ -1,5 +1,6 @@
-import io
+import threading
 import numpy as np
+import time
 from picamera2 import Picamera2
 from libcamera import controls
 
@@ -10,33 +11,47 @@ class Camera:
 
     cam: Picamera2
 
-    def __init__(self):
+    def __init__(self, resolution=(640, 480)):
         self.cam = Picamera2()
-        self.cam.resolution = (720, 480)
+        self.cam.resolution = resolution
+
+        self.frame = (0, None)
+        self.running = False
+        self.thread = None
+
+    def _capture(self):
+        while self.running:
+            self.frame = (time.time(), self.cam.capture_array())
 
     def start_cap(self):
         '''
         Start capturing video
         '''
         cfg = self.cam.create_video_configuration(main={
-            'size': (720, 480),
+            'size': self.cam.resolution,
             'format': 'RGB888'
             })
         self.cam.configure(cfg)
         self.cam.start()
         self.cam.set_controls({'AfMode': controls.AfModeEnum.Continuous})
+
+        self.running = True
+        self.thread = threading.Thread(target=self._capture)
+        self.thread.start()
     
     def stop_cap(self):
         '''
         Stop capturing video
         '''
 
+        self.running = False
+        self.thread.join()
         self.cam.stop()
+        self.thread = None
     
     def get_frame(self) -> np.ndarray:
         '''
         Grab a numpy array
         '''
 
-        arr = self.cam.capture_array()
-        return arr
+        return self.frame
--- crescendo_vision/main.py
+++ crescendo_vision/main.py
@@ -1,6 +1,8 @@
 from camera import Camera
 from notedetector import NoteDetector
 from nts import NT
+import multiprocessing
+import time
 
 print('FRC Team 4533 "Phoenix" -- 2024 Crescendo Vision')
 
@@ -15,36 +17,55 @@ cam = Camera()
 
 # Start capturing I guess
 cam.start_cap()
- 
-while True:
-    #FOR TESTING ONLY
-    #img = cv2.imread('samples/3notes1robot.jpg')
-    #rgb = cv2.cvtColor(img, cv2.COLOR_YUV2RGB)
-    #cv2.imwrite('test.png', img=img)
-    
-    while True:
-        # Get a frame from the camera
-        frame = cam.get_frame()
-        if frame is not None:
-            print('got frame')
 
+def process_frame(frame_queue, result_queue):
+    while True:
+        ctime, frame = frame_queue.get()
+        if frame is not None:
             # Run note detector on frame
             notes = ai.process_frame(frame)
-    
+
             # If any notes were detected...
             if len(notes) > 0:
-                nt.putBool('note', True)
+                result_queue.put((True, ctime, notes))
+            else:
+                result_queue.put((False, ctime, []))
+
+if __name__ == '__main__':
+    # Create queues for communication between processes
+    frame_queue = multiprocessing.Queue()
+    result_queue = multiprocessing.Queue()
+
+    # Start a separate process for processing frames
+    process = multiprocessing.Process(target=process_frame, args=(frame_queue, result_queue))
+    process.start()
+
+    while True:
+        # Get a frame from the camera
+        ctime, frame = cam.get_frame()
+
+        if frame is not None:
+            # Put the frame in the queue for processing
+            frame_queue.put((ctime, frame))
+
+            # Get the result from the queue
+            note_detected, ctime, notes = result_queue.get()
+            elapsed = time.time() - ctime
+
+            nt.putBool('note', note_detected)
+            nt.putInt('elapsed', elapsed)
+
+            if note_detected:
                 # Sort by distance to note (area of the bounding box) in descending order
                 notes.sort(key=lambda a: a['weight'], reverse=True)
-    
+
                 # Take the closest one
                 closestNote = notes[0]
-    
+
                 print(closestNote)
+
                 # Let auto know via nettables
                 nt.putInt('x', closestNote['x'])
                 nt.putInt('y', closestNote['y'])
                 nt.putInt('width', closestNote['width'])
                 nt.putInt('height', closestNote['height'])
-            else:
-                nt.putBool('note', False)
