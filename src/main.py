7af7bb2e630478d29495235a1fadd6b6d81a67a4
drakeerv <drakeerv@outlook.com>
2024-03-27 02:42:47 -0400

    Make threaded network tables

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from camera import Camera
 from ultralytics import YOLO
 from nts import NT
 import multiprocessing
+import threading
 import os
 
 print('FRC Team 4533 "Phoenix" -- 2024 Crescendo Vision')
@@ -18,6 +19,30 @@ def process_frame(input_queue: multiprocessing.Queue, output_queue: multiprocess
             objs = MODEL.predict(img, conf=CONF_THRESHOLD, verbose=False)[0]
             output_queue.put(objs.cpu().numpy().boxes.data)
 
+def handle_objects(objects_queue: multiprocessing.Queue, nt: NT):
+    while True:
+        if not objects_queue.empty():
+            objects = [box for box in objects_queue.get() if box[5] == 0]
+            nt.putBool('note', len(objects) > 0)
+
+            if len(objects) == 0:
+                continue
+
+            objects.sort(key=lambda a: a[4], reverse=True)
+
+            box = objects[0]
+            centerX = (box[0] + box[2]) / 2
+            centerY = (box[1] + box[3]) / 2
+            width = box[2] - box[0]
+            height = box[3] - box[1]
+            confidence = box[4]
+
+            nt.putNumber('x', centerX)
+            nt.putNumber('y', centerY)
+            nt.putNumber('width', width)
+            nt.putNumber('height', height)
+            nt.putNumber('confidence', confidence)
+
 def main():
     # Initialize nt
     nt = NT()
@@ -33,8 +58,10 @@ def main():
     output_queue = multiprocessing.Queue(maxsize=1)
 
     # Start a separate process for processing frames
-    process = multiprocessing.Process(target=process_frame, args=(input_queue, output_queue), daemon=True)
-    process.start()
+    multiprocessing.Process(target=process_frame, args=(input_queue, output_queue), daemon=True).start()
+
+    # Start a separate process for handling objects
+    threading.Thread(target=handle_objects, args=(output_queue, nt), daemon=True).start()
 
     while True:
         # Get a frame from the camera
@@ -44,27 +71,6 @@ def main():
             # print(frame.shape)
             # Put the frame in the queue for processing
             input_queue.put(frame)
-
-            # Get the result from the queue
-            objects = output_queue.get()
-            print(objects)
-
-            # nt.putBool('note', note_detected)
-
-            # if note_detected:
-            #     # Sort by distance to note (area of the bounding box) in descending order
-            #     notes.sort(key=lambda a: a['weight'], reverse=True)
-
-            #     # Take the closest one
-            #     closestNote = notes[0]
-
-            #     print(closestNote)
-
-            #     # Let auto know via nettables
-            #     nt.putInt('x', closestNote['x'])
-            #     nt.putInt('y', closestNote['y'])
-            #     nt.putInt('width', closestNote['width'])
-            #     nt.putInt('height', closestNote['height'])
 
 if __name__ == '__main__':
     main()
