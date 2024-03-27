29771bc6ac291d2d1d62b8acf56b442764032825
drakeerv <drakeerv@outlook.com>
2024-03-27 00:21:29 -0400

    Improve install script

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 # install coral usb driver
+echo "Installing Coral USB driver"
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update && sudo apt-get install libedgetpu1-max -y
 
 # install python
+echo "Installing Python"
 sudo apt install git gcc make -y
 sudo apt install libreadline-dev libbz2-dev libssl-dev libncurses5-dev libffi-dev libsqlite3-dev liblzma-dev -y
 
@@ -11,17 +13,20 @@ curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+source ~/.bashrc
 
 pyenv install 3.9.18
 pyenv local 3.9.18
 
 # install packages
+echo "Installing packages"
 sudo apt install libcap-dev libcamera-dev pkg-config g++ -y
 pip install --upgrade pip
 pip install --extra-index-url https://google-coral.github.io/py-repo/ pycoral~=2.0
 pip install -r requirements.txt
 
-# copy install/vision.service to /etc/systemd/system/vision.service
+# install service
+echo "Installing service"
 sudo cp install/vision.service /etc/systemd/system/vision.service
 sudo sed -i "s|%path%|$(pwd)|g" /etc/systemd/system/vision.service
 sudo chmod 644 /etc/systemd/system/vision.service
